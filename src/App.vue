1a3639830d192f356e4f13db033d6c7d01da7bbc
林佳俊 <alinjiajun@163.com>
2018-03-22 10:48:00 +0800

    提交多入口项目基础版本文件

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>app</h1>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'asdf'
+    }
+  },
+  created() {
+    this.$http.get('/po').then(res => {
+      console.log(res.data);
+    }).catch(res =>{
+      console.log(res.data);
+    })
+    this.$http.get('/baidu').then(res => {
+      console.log(res.data);
+    }).catch(res =>{
+      console.log(res.data);
+    })
+    this.$http.get('/blo').then(res => {
+      console.log(res.data);
+    }).catch(res =>{
+      console.log(res.data);
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
